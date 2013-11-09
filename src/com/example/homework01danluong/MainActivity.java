9832d9bcc2c9e4742a9ec00064de586474d555c9
Dan Luong <dan_luong@hotmail.com>
2013-11-09 08:32:28 -0800

    Start Addition of 2.3 compatible action bar

--- src/com/example/homework01danluong/MainActivity.java
+++ src/com/example/homework01danluong/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.homework01danluong;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
